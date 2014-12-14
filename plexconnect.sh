c5524f3198a1c0d8ca11feda24e7c8ff5ddf7bb1
pducharme <pducharme@gmail.com>
2014-12-14 16:34:48 -0500

    Update plexconnect.sh

--- plexconnect.sh
+++ plexconnect.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# Does the user specify a DNS
+if [ -z "$DNS" ]; then
+  echo "No user-specified DNS IP address, using Google DNS IP address"
+else
+  sed -i 's/ip_dnsmaster = 8.8.8.8/ip_dnsmaster =$DNS/g' /opt/plexconnect/settings.cfg
+fi
+
 exec /usr/bin/python /opt/plexconnect/PlexConnect.py
