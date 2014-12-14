1eddc9f5bcfa1684a86677d4ff574fda421a9c8a
pducharme <pducharme@gmail.com>
2014-12-14 18:03:01 -0500

    Update plexconnect.sh

--- plexconnect.sh
+++ plexconnect.sh
@@ -6,7 +6,7 @@ if [ -z "$DNS" ]; then
   exec /usr/bin/python /opt/plexconnect/PlexConnect.py
 else
   exec /usr/bin/python /opt/plexconnect/PlexConnect.py
-  exec kill all
+  exec kill 'pgrep -f PlexConnect.py'
   sed -i 's/8.8.8.8/$DNS/g' /opt/plexconnect/Settings.cfg
   exec /usr/bin/python /opt/plexconnect/PlexConnect.py
 fi
