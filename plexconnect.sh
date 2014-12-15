0b5e220caf104a28f377242053716d0f4f82aa79
pducharme <pducharme@gmail.com>
2014-12-14 20:58:22 -0500

    Update plexconnect.sh

--- plexconnect.sh
+++ plexconnect.sh
@@ -5,9 +5,11 @@ if [ -z "$DNS" ]; then
   echo "No user-specified DNS IP address, using Google DNS IP address"
   exec /usr/bin/python /opt/plexconnect/PlexConnect.py
 else
-  echo "User specified a DNS IP address, will use it in PlexConnect"
+  echo "DNS Variable exist, will use it in PlexConnect"
   /usr/bin/python /opt/plexconnect/Settings.py
+  echo "Replacing ip_dnsmaster=8.8.8.8 by ip_dnsmaster=$DNS in Settings.cfg file..."
   sed -i 's/8.8.8.8/'$DNS'/g' /opt/plexconnect/Settings.cfg
+  echo "Launching PlexConnect with the new Settings.cfg..."
   exec /usr/bin/python /opt/plexconnect/PlexConnect.py
 fi
 
