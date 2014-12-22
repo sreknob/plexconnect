c8d964916a9f7f4d36215da36f644ead4ccf5293
Patrick Ducharme <pducharme@gmail.com>
2014-12-21 23:25:32 -0500

    Update plexconnect.sh

--- plexconnect.sh
+++ plexconnect.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
+
+
 # Check if existing config file exist, if so will use it.
-if [ -f /config/Settings.cfg ]; then
-  echo "Using existing config file."
-  ls -s /opt/plexconnect/Settings.cfg /config/Settings.cfg
-else #if not, create a new one.
-  echo "Creating new default config file."
   /usr/bin/python /opt/plexconnect/Settings.py
   mv /opt/plexconnect/Settings.cfg /opt/plexconnect/Settings-backup.cfg
   ls -s /opt/plexconnect/Settings.cfg /config/Settings.cfg
-  mv /opt/plexconnect/Settings-backup.cfg /config/Settings.cfg
+  if [ -f /config/Settings.cfg ]; then
+    echo "Using existing config file."
+  else
+    mv /opt/plexconnect/Settings-backup.cfg /config/Settings.cfg
   chown nobody:users /config/Settings.cfg
-fi
+  fi
 
 # Run the PlexConnect  
 echo "*** Launching PlexConnect with the new Settings.cfg..."
