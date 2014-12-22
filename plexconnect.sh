9b38024a6fae24d06e5e96d803c4c8d5f0fab254
Patrick Ducharme <pducharme@gmail.com>
2014-12-22 17:28:46 -0500

    Update plexconnect.sh

--- plexconnect.sh
+++ plexconnect.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 # Check if existing config file exist, if so will use it.
- 
-  if [ -f /config/Settings.bak ]; then 
-    mv /config/Settings.bak /config/Settings.cfg
-  fi
   
  echo "*** Check if previous config exist, if so will use it..."  
   if [ -f /config/Settings.cfg ]; then   
-    cp /config/Settings.cfg /config/Settings.bak
-    mv /config/Settings.cfg /opt/plexconnect/Settings.cfg
+    cp /config/Settings.cfg /opt/plexconnect/Settings.cfg
   else
     /usr/bin/python /opt/plexconnect/Settings.py 
   fi
-  echo "*** Creating symlink to config..."
-  ln -s /opt/plexconnect/Settings.cfg /config/Settings.cfg
 
 # Run the PlexConnect  
 echo "*** Launching PlexConnect..."
