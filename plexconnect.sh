46c0fa39430eebfb84f2b32c1a355ef1daea8996
Patrick Ducharme <pducharme@gmail.com>
2014-12-21 23:10:37 -0500

    Update plexconnect.sh

--- plexconnect.sh
+++ plexconnect.sh
@@ -1,36 +1,17 @@
 #!/bin/bash
 
-#Creating Default Settings.cfg 
+# Check if existing config file exist, if so will use it.
+if [ -f /config/Settings.cfg ]; then
+  echo "Using existing config file."
+  ls -s /opt/plexconnect/Settings.cfg /config/Settings.cfg
+else #if not, create a new one.
+  echo "Creating new default config file."
   /usr/bin/python /opt/plexconnect/Settings.py
-
-# Does the user specify a DNS
-if [ -z "$DNS" ]; then
-  echo "*** No user-specified DNS IP address, using Google DNS IP address"
-else
-  echo "*** Replacing ip_dnsmaster=8.8.8.8 by ip_dnsmaster=$DNS in Settings.cfg file..."
-  sed -i 's/8.8.8.8/'$DNS'/g' /opt/plexconnect/Settings.cfg
+  mv /opt/plexconnect/Settings.cfg /opt/plexconnect/Settings-backup.cfg
+  ls -s /opt/plexconnect/Settings.cfg /config/Settings.cfg
+  mv /opt/plexconnect/Settings-backup.cfg /config/Settings.cfg
+  chown nobody:users /config/Settings.cfg
 fi
-
-# Does the user specified a Alternate HTTP Port ?
-if [ -z "$HTTP_Port" ]; then
-  echo "*** No specific HTTP Port specified, using default (80)..."
-else
-  echo "*** A Specific HTTP Port has been specified, using ('$HTTP_Port')..."
-  sed -i 's/80/'$HTTP_Port'/g' /opt/plexconnect/Settings.cfg
-fi  
-
-# Does the user specified a Alternate HTTPS Port ?  
-if [ -z "$HTTPS_Port" ]; then
-  echo "*** No specific HTTPS Port specified, using default (443)..."
-else
-  echo "*** A Specific HTTPS Port has been specified, using ('$HTTPS_Port')..."
-  sed -i 's/443/'$HTTPS_Port'/g' /opt/plexconnect/Settings.cfg
-fi  
-
-#Move Settings out of the docker container
-mv /opt/plexconnect/Settings.cfg /opt/plexconnect/Settings-backup.cfg
-ls -s /opt/plexconnect/Settings.cfg /config/Settings.cfg
-mv /opt/plexconnect/Settings-backup.cfg /config/Settings.cfg
 
 # Run the PlexConnect  
 echo "*** Launching PlexConnect with the new Settings.cfg..."
